0999364167975e5e24dc372380eb24506341d210
Dany <89309539+danbenba@users.noreply.github.com>
2026-03-18 14:08:10 +0100

    fix: build errors

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -4,7 +4,6 @@ const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   async rewrites() {
     return [
       {
